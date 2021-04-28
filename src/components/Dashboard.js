9112a37c6b70b8aea9e6269971a37285d024cbe7
Leighton Chien <leightonchien@gmail.com>
2021-04-27 19:31:44 -0700

    add data to panels

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 import Loading from "./Loading";
+import Panel from "./Panel";
 
 const data = [
 	{
@@ -28,16 +29,29 @@ const data = [
 
 class Dashboard extends Component {
   
-  state = { loading: false };
+  state = { 
+    
+          loading: false 
+  };
 
 	render() {
+      
     const dashboardClasses = classnames("dashboard");
 
     if (this.state.loading) {
 			return <Loading />;
 		}
 
-		return <main className={dashboardClasses} />;
+		const panels = data.map(panel => (
+			<Panel
+				key={panel.id}
+				id={panel.id}
+				label={panel.label}
+				value={panel.value}
+			/>
+		));
+
+		return <main className={dashboardClasses}>{panels}</main>;
 	}
 }
 
